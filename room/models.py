a1d0b1394736a862eba9a1ba155917a7f3821986
Aditya Singh <96740553+AdityaSingh21074@users.noreply.github.com>
2024-01-14 14:09:32 +0530

    making of user rooms

--- room/models.py
+++ room/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.name
 
 
 class Message(models.Model):
